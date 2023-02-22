d2ac03ca8bf07fd4dea9481adb7afdeadb1fa3b7
aryar <arya.rizky4@gmail.com>
2023-02-22 13:30:11 +0700

    adding cart reducer

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+import { useState } from "react";
 
 const MealItemForm = (props) => {
+  const [isValid, setIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const amountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      amountNumber < 1 ||
+      amountNumber > 10
+    ) {
+      setIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(amountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -16,6 +40,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!isValid && <p>Enter valid amount (1-10)</p>}
     </form>
   );
 };
